577a864e40cea2f83a937c1b70d15d935cc208e7
EdoFA12 <edo.broo12@gmail.com>
2025-03-12 00:54:34 +0700

    Initial commit

---
+++ app/src/main/java/com/edofa/projectlogin/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.edofa.projectlogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val register = findViewById<TextView>(R.id.register)
+        register.setOnClickListener {
+            Toast.makeText(this, "Mardin Kece", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivityRegister::class.java))
+        }
+
+
+
+
+    }
+
+}
+
+
+
